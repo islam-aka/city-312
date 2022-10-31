f66e032ef5282d6300db6edc40e989c259a9068e
islam-aka <islamjan.m.m@gmail.com>
2022-10-31 15:47:07 +0600

    test

--- pages/partner.tsx
+++ pages/partner.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { AiOutlineHeart } from 'react-icons/ai'
 
 const partner = () => {
-	return <div>partner</div>
+	return (
+		<div>
+			partner
+			<AiOutlineHeart color="red" width={'50px'} height={'50px'} />
+		</div>
+	)
 }
 
 export default partner
