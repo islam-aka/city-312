d1b4a7147d8bb4a51a197c43cd10d6cf57daa4fb
islam-aka <islamjan.m.m@gmail.com>
2022-11-22 14:54:44 +0600

    v2.002

--- app/components/layout/UserProfile/UserProfile.tsx
+++ app/components/layout/UserProfile/UserProfile.tsx
@@ -11,141 +11,159 @@ import favourites from '@/assets/svg/favourites.svg'
 import message from '@/assets/svg/message.svg'
 import notifications from '@/assets/svg/notifications.svg'
 import usageHistory from '@/assets/svg/usageHistory.svg'
+import advertising from '@/assets/images/Advertising.png'
 
 import styles from './userProfile.module.scss'
+import { IUser } from '../UserCard/userCard.interface'
 
-const UserProfile: FC = () => {
+const UserProfile: FC<IUser> = ({ data }) => {
 	const router = useRouter()
 
 	return (
-		<div className={styles.userProfile}>
-			<h2 className="font-semibold text-[18px] leading-[21px]">мой профиль</h2>
-			<div className="flex mt-5">
-				<Image
-					width={45}
-					height={45}
-					src={user}
-					alt="City-312"
-					draggable={false}
-				/>
-				<div className="ml-[25px]">
-					<p className="font-semibold text-[16px] leading-[19px]">
-						Жамшитов Ж.
-					</p>
-					<p className="text-[16px] leading-[19px]">пользователь</p>
+		<>
+			<div className={styles.userProfile}>
+				<h2 className="font-semibold text-[18px] leading-[21px]">
+					мой профиль
+				</h2>
+				<div className="flex mt-5">
+					<Image
+						width={45}
+						height={45}
+						src={user}
+						alt="City-312"
+						draggable={false}
+					/>
+					<div className="ml-[25px]">
+						<p className="font-semibold text-[16px] leading-[19px]">
+							{`${data.second_name} ${data.name}`}.
+						</p>
+						<p className="text-[16px] leading-[19px]">пользователь</p>
+					</div>
+				</div>
+				<div className="mt-[15px] mb-6 flex gap-2">
+					<Btn
+						style={{
+							width: '124px',
+							padding: '7px 5px',
+							background: 'linear-gradient(0deg, #7EC9F5 0%, #3957ED 97.5%)',
+							borderRadius: '4.46108px',
+							color: 'white',
+						}}
+					>
+						+996 555 55 55 55
+					</Btn>
+					<Btn
+						style={{
+							width: '124px',
+							padding: '7px 5px',
+							background: 'linear-gradient(0deg, #7EC9F5 0%, #3957ED 97.5%)',
+							borderRadius: '4.46108px',
+							color: 'white',
+						}}
+					>
+						Редактировать
+					</Btn>
+				</div>
+				<div className={styles.userProfile__btn}>
+					<Link href="/userCard">
+						<a
+							className={
+								router.pathname === '/userCard' ? styles.active : styles.none
+							}
+						>
+							<Image
+								width={18}
+								height={18}
+								src={card}
+								alt="City-312"
+								draggable={false}
+							/>
+							<span>Моя карта</span>
+						</a>
+					</Link>
+
+					<Link href="/favourites">
+						<a
+							className={
+								router.pathname === '/favourites' ? styles.active : styles.none
+							}
+						>
+							<Image
+								width={18}
+								height={18}
+								src={favourites}
+								alt="City-312"
+								draggable={false}
+							/>
+							<span>Избранное</span>
+						</a>
+					</Link>
+
+					<Link href="/notifications">
+						<a
+							className={
+								router.pathname === '/notifications'
+									? styles.active
+									: styles.none
+							}
+						>
+							<Image
+								width={18}
+								height={18}
+								src={notifications}
+								alt="City-312"
+								draggable={false}
+							/>
+							<span>Уведомления</span>
+							<span className={styles.info}>4</span>
+						</a>
+					</Link>
+					<Link href="/message">
+						<a
+							className={
+								router.pathname === '/message' ? styles.active : styles.none
+							}
+						>
+							<Image
+								width={18}
+								height={18}
+								src={message}
+								alt="City-312"
+								draggable={false}
+							/>
+							<span>Сообщения</span>
+							<span className={styles.info}>3</span>
+						</a>
+					</Link>
+					<Link href="/usageHistory">
+						<a
+							className={
+								router.pathname === '/usageHistory'
+									? styles.active
+									: styles.none
+							}
+						>
+							<Image
+								width={18}
+								height={18}
+								src={usageHistory}
+								alt="City-312"
+								draggable={false}
+							/>
+							<span>История пользования</span>
+						</a>
+					</Link>
 				</div>
 			</div>
-			<div className="mt-[15px] mb-6 flex gap-2">
-				<Btn
-					style={{
-						width: '124px',
-						padding: '7px 5px',
-						background: 'linear-gradient(0deg, #7EC9F5 0%, #3957ED 97.5%)',
-						borderRadius: '4.46108px',
-						color: 'white',
-					}}
-				>
-					+996 555 55 55 55
-				</Btn>
-				<Btn
-					style={{
-						width: '124px',
-						padding: '7px 5px',
-						background: 'linear-gradient(0deg, #7EC9F5 0%, #3957ED 97.5%)',
-						borderRadius: '4.46108px',
-						color: 'white',
-					}}
-				>
-					Редактировать
-				</Btn>
+			<div className="mt-5 hidden lg:block">
+				<Image
+					width={299}
+					height={299}
+					src={advertising}
+					alt="Advertising"
+				></Image>
 			</div>
-			<div className={styles.userProfile__btn}>
-				<Link href="/userProfile">
-					<a
-						className={
-							router.pathname === '/userProfile' ? styles.active : styles.none
-						}
-					>
-						<Image
-							width={18}
-							height={18}
-							src={card}
-							alt="City-312"
-							draggable={false}
-						/>
-						<span>Моя карта</span>
-					</a>
-				</Link>
-
-				<Link href="/favourites">
-					<a
-						className={
-							router.pathname === '/favourites' ? styles.active : styles.none
-						}
-					>
-						<Image
-							width={18}
-							height={18}
-							src={favourites}
-							alt="City-312"
-							draggable={false}
-						/>
-						<span>Избранное</span>
-					</a>
-				</Link>
-
-				<Link href="/notifications">
-					<a
-						className={
-							router.pathname === '/notifications' ? styles.active : styles.none
-						}
-					>
-						<Image
-							width={18}
-							height={18}
-							src={notifications}
-							alt="City-312"
-							draggable={false}
-						/>
-						<span>Уведомления</span>
-						<span className={styles.info}>4</span>
-					</a>
-				</Link>
-				<Link href="/message">
-					<a
-						className={
-							router.pathname === '/message' ? styles.active : styles.none
-						}
-					>
-						<Image
-							width={18}
-							height={18}
-							src={message}
-							alt="City-312"
-							draggable={false}
-						/>
-						<span>Сообщения</span>
-						<span className={styles.info}>3</span>
-					</a>
-				</Link>
-				<Link href="/usageHistory">
-					<a
-						className={
-							router.pathname === '/usageHistory' ? styles.active : styles.none
-						}
-					>
-						<Image
-							width={18}
-							height={18}
-							src={usageHistory}
-							alt="City-312"
-							draggable={false}
-						/>
-						<span>История пользования</span>
-					</a>
-				</Link>
-			</div>
-		</div>
+		</>
 	)
 }
 
--- pages/userCard.tsx
+++ pages/userCard.tsx
@@ -1,80 +1,21 @@
-import React from 'react'
-
+import React, { FC } from 'react'
 import Footer from '@/components/layout/Footer/Footer'
 import Header from '@/components/layout/Header/Header'
 import UserProfile from '@/components/layout/UserProfile/UserProfile'
-import Btn from '@/components/ui/Btn/Btn'
-
+import UserCards from '@/components/layout/UserCard/UserCard'
 import styles from './userCard.module.scss'
+import { userProfile } from '@/components/layout/UserCard/userCard.data'
 
-const UserCard = () => {
+const UserCard: FC = () => {
 	return (
 		<>
 			<Header />
 			<div className={styles.userCard}>
-				<div className="flex flex-col">
-					<div>
-						<UserProfile />
-					</div>
-					<div></div>
+				<div>
+					<UserProfile data={userProfile} />
 				</div>
-				<div className={styles.userCard__info}>
-					<div className={styles.userCard__bg}></div>
-					<div>
-						<div className="flex lg:gap-[30px] gap-[15px] mb-6  mt-8 ">
-							<div>
-								<p className="text-[#545454] text-[14px] leading-[130%]">
-									Мои баллы
-								</p>
-								<p className={styles.bg}>100500 б.</p>
-							</div>
-							<div>
-								<p className="text-[#545454] text-[14px] leading-[130%]">
-									Сэкономлено
-								</p>
-								<p className={styles.bg}>15000 сом</p>
-							</div>
-						</div>
-						<div className="flex lg:gap-[30px] gap-[15px]">
-							<div className="lg:w-[178px] w-[135px]">
-								<p className="text-[#545454] text-[14px] leading-[130%] mb-[5px]">
-									№ карты
-								</p>
-								<p className="lg:py-[12px] lg:px-[14px] py-[10px] px-[5px] rounded-[4px] border-black-900 border border-solid font-semibold lg:text-[16px] text-[14px] lg:leading-[19px] leading-4 ">
-									0000 0312 000 001
-								</p>
-							</div>
-							<div className="">
-								<p className="text-[#545454] text-[14px] md:mb-[21px] mb-[8px] leading-[130%] lg:w-[178px] w-[154px]">
-									Обладатель
-								</p>
-								<p className="md:w-[200px] w-[135px]  lg:text-[16px] lg:leading-[19px] text-[14px] leading-[16px] font-semibold">
-									Жамшитов Жаныш Мухаметрасулович
-								</p>
-							</div>
-						</div>
-						<div className="flex lg:gap-[30px] gap-[15px] mt-6 ">
-							<div className="lg:w-[178px] ">
-								<p className=" text-[#545454] text-[14px] leading-[130%] mb-1">
-									Куплено
-								</p>
-								<p className="font-semibold lg:text-[16px] text-[14px] lg:leading-[19px] leading-[16px] lg:w-[178px] w-[135px]">
-									01.05.2022
-								</p>
-							</div>
-							<div>
-								<p className="text-[#545454] text-[14px] leading-[130%] mb-1">
-									Действителен до
-								</p>
-								<p className="font-semibold lg:text-[16px] text-[14px] lg:leading-[19px] leading-[16px]">
-									01.05.2023
-								</p>
-							</div>
-						</div>
-						<div className="md:block inline-block mt-5   ">
-							<Btn>Продлить</Btn>
-						</div>
-					</div>
+				<div>
+					<UserCards data={userProfile} />
 				</div>
 			</div>
 			<Footer />
