822e032ffa14d9d822717027d3dfc0525d57af51
islam-aka <islamjan.m.m@gmail.com>
2022-12-13 16:44:00 +0600

    fixed v2.2

--- pages/stocksPages.tsx
+++ pages/stocksPages.tsx
@@ -1,7 +1,7 @@
 import Stocks from '@/components/layout/Stocks/Stocks'
 import { stocksCard } from '@/components/layout/Stocks/stocks.data'
 import React from 'react'
-import styles from './StocksPages.module.scss'
+import styles from './stocksPages.module.scss'
 
 const StocksPages = () => {
 	return (
